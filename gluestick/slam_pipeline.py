3e9551bd3dcb1f93dc2bb98e03da002379b1fa9e
c-keil <cwk.321@gmail.com>
2024-03-25 22:53:59 -0400

    cleanup debug statements

--- gluestick/slam_pipeline.py
+++ gluestick/slam_pipeline.py
@@ -23,7 +23,7 @@ def numpy_to_batch(batch, device):
 class SLAM_Pipeline():
     '''thin wrapper around gluestick to make integration to c++ more convenient'''
     def __init__(self):
-        print("Python Side -- __init__ SLAM_Pipeline()")
+        # print("Python Side -- __init__ SLAM_Pipeline()")
         MAX_N_POINTS, MAX_N_LINES = 1000, 300
         self.image_counter = 0 #debug
         # Evaluation config
@@ -73,21 +73,6 @@ class SLAM_Pipeline():
         pred = batch_to_np(pred)
         if transpose:
             pred["descriptors"] = pred["descriptors"].T.copy(order = 'C')
-        if np.any(pred["keypoints"] < 0) or np.any(pred["keypoints"] > np.array((639,511)).reshape(1,2)):
-            print("python -- BAD KEYPOINT")
-            p = "/home/colin/Research/ir/slam_ws/tmp"
-            cv2.imwrite(p + "/debug_image_{}.png".format(self.image_counter), image)
-            print(pred["keypoints"][pred["keypoints"] > np.array((639,511)).reshape(1,2)] )
-        # print("Python: descriptors shape : {}".format(pred["descriptors"].shape))
-        # print("Python: extracted {} descriptors".format(len(pred["descriptors"])))
-        # print("Desc first row: {}...".format(pred["descriptors"][0,:10]))
-        # print("Desc last row: {}...".format(pred["descriptors"][-1,:10]))
-        # # print("Flags:")
-        # # print(pred["descriptors"].flags)
-        # #debug stuff
-        # p = "/home/colin/Research/ir/slam_ws/tmp"
-        # cv2.imwrite(p + "/debug_image_{}.png".format(self.image_counter), image)
-        # self.image_counter += 1 
         return pred
     
     def copy_numpy(tensor):
@@ -95,18 +80,13 @@ class SLAM_Pipeline():
     
     def detect_extract2(self, image):
         '''expects a numpy array image'''
-        # print("Python side -- detect_extract2()")
         #convert to torch 
         torch_image = numpy_image_to_torch(image)
         torch_image = torch_image.to(self.device)[None]
         #run get kps, lines, descs
         pred = self.pipeline._extract_single_forward(torch_image)
-        
-        #debug
-        # pred["image_debug"] = image
 
         #get numpy versions of everything
-        # pred = batch_to_np(pred)
         descs = np.ascontiguousarray(pred["descriptors"].clone().detach().cpu().numpy()[0].T)
         kps = np.ascontiguousarray(pred["keypoints"].clone().detach().cpu().numpy()[0])
         prediction = {"gs_frame_id":self.next_frame_id, "descriptors":descs, "keypoints":kps}
@@ -119,7 +99,6 @@ class SLAM_Pipeline():
     
     def match2(self, frame_id1, frame_id2, reloc1 = False, reloc2 = False, return_full_batch = False):
         '''runs matching against frames stored in python dict. reloc# attempts to use frames stored in the relocalization frames'''
-        # print("Python Side -- match2: frame {} against frame {}".format(frame_id1, frame_id2))
         if not reloc1:
             pred1 = self.frames[frame_id1]
         else:
@@ -131,9 +110,9 @@ class SLAM_Pipeline():
         pred = self.pipeline._match_forward(self.pipeline._merge_kp_pred(pred1, pred2))
         matches = pred["matches0"].clone().detach()[0].tolist()
         match_scores = pred["match_scores0"].clone().detach()[0].cpu().numpy()
-        # self.draw_debug_match_image(pred, frame_id1,frame_id2,reloc1,reloc2)
+
         match_distances = 1 - match_scores
-        # print("python side -- match2: first matches: {}".format(matches[:10]))
+
         if return_full_batch:
             return batch_to_np(pred)
         return matches, match_distances.tolist()
@@ -153,9 +132,9 @@ class SLAM_Pipeline():
         matched_kps1 = kp1[match_indices]
         keypoints0 = [cv2.KeyPoint(kp[0], kp[1], 1) for kp in kp0]
         keypoints1 = [cv2.KeyPoint(kp[0], kp[1], 1) for kp in kp1]
-        # matches = [cv2.DMatch(i,i,0.5) for i in range(len(keypoints0))]
+
         good = [cv2.DMatch(i,j,0.0) for i,j in enumerate(m0) if j != -1]
-        # print(len(keypoints0), len(keypoints1), matches)
+
         match_im = cv2.drawMatches(im0, keypoints0, im1, keypoints1, good, None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
         name = '/home/colin/Research/ir/slam_ws/mapping_results_tmp/debug_ims/' + "{}_{}_{}_{}_{}.png".format(self.debug_counter,i,j,a,b)
         cv2.imwrite(name,match_im)
@@ -181,63 +160,3 @@ class SLAM_Pipeline():
         '''reads pickled db'''
         with open(fname,'br') as pf:
             self.reloaclization_frames = pickle.load(pf)
-        
-
-if __name__ == "__main__":
-    
-    import cv2
-
-    slam_pipe = SLAM_Pipeline()
-
-    
-    img_path0 = "/media/colin/box_data/ir_data/nuance_data/kri_day_2/cam_3/matlab_clahe2/1689805041743999958.png"
-    img_path1 = "/media/colin/box_data/ir_data/nuance_data/kri_night/cam_3/matlab_clahe2/1689819945923000097.png"
-    gray0 = cv2.imread(img_path0, cv2.IMREAD_GRAYSCALE)
-    gray1 = cv2.imread(img_path1, cv2.IMREAD_GRAYSCALE)
-
-    # import matplotlib.pyplot as plt
-    # f, axarr = plt.subplots(1, 2)
-    # axarr[0].imshow(gray0, cmap='gray')
-    # axarr[1].imshow(gray1, cmap='gray')
-    # plt.show()
-
-    # torch_gray0, torch_gray1 = numpy_image_to_torch(gray0), numpy_image_to_torch(gray1)
-    # torch_gray0, torch_gray1 = torch_gray0.to(slam_pipe.device)[None], torch_gray1.to(slam_pipe.device)[None]
-
-    # pred0 = slam_pipe.detect_extract(gray0)
-    # pred1 = slam_pipe.detect_extract(gray1)
-    # print("kp detection keys")
-    # for k, v in pred0.items():
-    #     print("key : {} , type : {}, shape {}".format(k, type(v), v.shape if type(v) == np.ndarray else -1))
-    # print(pred0["keypoints"][0])
-    # print("descriptor row:\n{}".format(pred0["descriptors"][0,:10]))
-
-    # pred = slam_pipe.match(pred0, pred1)
-    # print("descriptor row:\n{}".format(pred0["descriptors"][0,0,:10]))
-    # print("descriptor row:\n{}".format(pred["descriptors0"][0,:10]))
-    # print("match result keys")
-    # print(pred.keys())
-    # for k, v in pred.items():
-    #     print("key : {} , type : {}, shape {}".format(k, type(v), v.shape if type(v) == np.ndarray else -1))
-    # print(pred["matches0"])
-    
-    pred0 = slam_pipe.detect_extract2(gray0)
-    pred1 = slam_pipe.detect_extract2(gray1)
-    matches, match_scores = slam_pipe.match2(pred0["gs_frame_id"], pred1["gs_frame_id"])
-    # print(matches[:10])
-    # print(match_scores[:10])
-    
-    # har = cv2.cornerHarris(gray0,10,5,0.001)
-    # print("min :", har.min())
-    # print("max :", har.max())
-    # print("mean: ", har.mean())
-    # har = cv2.dilate(har,None)
-    # # har[har<0.01] = 0.0
-
-    # import matplotlib.pyplot as plt
-    # rbgim = cv2.cvtColor(gray0, cv2.COLOR_GRAY2BGR)
-    # rbgim[har > 0.01] = [0,0,255]
-    # fig, ax = plt.subplots()
-    # ax.imshow(rbgim)
-    # plt.show()
-
